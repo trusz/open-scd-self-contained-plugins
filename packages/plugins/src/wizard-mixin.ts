9e46e835867ec9ef7f54d98f49fb0154deb21286
Tamás Russ <tamas@soot5.com>
2025-08-28 08:56:30 +0200

    ref: wizardmixin

--- packages/plugins/src/wizard-mixin.ts
+++ packages/plugins/src/wizard-mixin.ts
@@ -7,7 +7,7 @@ import { WizardDialog } from '@openscd/open-scd/src/wizard-dialog.js';
 import '@openscd/open-scd/src/wizard-dialog.js';
 
 /**
- * Base class for plugins that manage their own dialogs directly
+ * Base class for plugins that manage their own wizards
  * without depending on oscd-wizard
  */
 export class WizardMixin extends LitElement {
@@ -17,41 +17,54 @@ export class WizardMixin extends LitElement {
 
   @query('wizard-dialog') wizardUI!: WizardDialog;
 
-  /**
-   * Handles wizard events by adding or removing wizards from the workflow
-   */
-  private onWizard(we: WizardEvent) {
-    const wizard = we.detail.wizard;
-    if (wizard === null) this.workflow.shift();
-    else if (we.detail.subwizard) this.workflow.unshift(wizard);
-    else this.workflow.push(wizard);
-    this.requestUpdate('workflow');
-    this.updateComplete.then(() =>
-      this.wizardUI?.updateComplete.then(() =>
-        this.wizardUI?.dialog?.updateComplete.then(() =>
-          this.wizardUI?.dialog?.focus()
-        )
-      )
-    );
+  protected renderWizardDialog(): TemplateResult {
+    return html`
+      <wizard-dialog .wizard=${this.workflow[0]?.() ?? []}></wizard-dialog>
+    `;
   }
 
-  /**
-   * Add event listeners for wizard events and editor actions
-   */
   connectedCallback(): void {
     super.connectedCallback();
-    this.addEventListener('wizard', this.onWizard.bind(this));
-    this.addEventListener('editor-action', () =>
-      this.wizardUI?.requestUpdate()
-    );
+    this.addEventListener('wizard', this.onWizard);
+    this.addEventListener('editor-action', this.onEditorAction);
   }
 
-  /**
-   * Renders the wizard dialog component
-   */
-  protected renderWizardDialog(): TemplateResult {
-    return html`<wizard-dialog
-      .wizard=${this.workflow[0]?.() ?? []}
-    ></wizard-dialog>`;
+  disconnectedCallback(): void {
+    super.disconnectedCallback();
+    this.removeEventListener('wizard', this.onWizard);
+    this.removeEventListener('editor-action', this.onEditorAction);
   }
+
+  private onWizard = async (we: WizardEvent) => {
+    we.stopImmediatePropagation();
+    we.stopPropagation();
+
+    const wizard = we.detail.wizard;
+    if (wizard === null) {
+      this.workflow.shift();
+    } else if (we.detail.subwizard) {
+      this.workflow.unshift(wizard);
+    } else {
+      this.workflow.push(wizard);
+    }
+
+    this.requestUpdate('workflow');
+
+    await this.updateComplete;
+    await this.wizardUI?.updateComplete;
+    await this.wizardUI.dialog?.updateComplete;
+    this.wizardUI.dialog?.focus();
+
+    // this.updateComplete.then(() =>
+    //   this.wizardUI?.updateComplete.then(() =>
+    //     this.wizardUI?.dialog?.updateComplete.then(() =>
+    //       this.wizardUI?.dialog?.focus()
+    //     )
+    //   )
+    // );
+  };
+
+  private onEditorAction = () => {
+    this.wizardUI?.requestUpdate();
+  };
 }
